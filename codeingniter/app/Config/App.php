1fa9603ec626751d597989d5e89e25f0e60bcdbb
Shayna27 <61741756+Shayna27@users.noreply.github.com>
2021-05-20 10:04:21 -0500

    Update App.php

--- codeingniter/app/Config/App.php
+++ codeingniter/app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://cerounodigital.com/btc06/shayna/codeingniter/public/LoginController';
+	public $baseURL = 'http://cerounodigital.com/btc06/shayna/codeingniter/public';
 
 	/**
 	 * --------------------------------------------------------------------------

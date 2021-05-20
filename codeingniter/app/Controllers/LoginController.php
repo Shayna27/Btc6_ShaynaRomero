4ddcd537aecc38c14cc9ba12260a8205902964b7
Shayna27 <61741756+Shayna27@users.noreply.github.com>
2021-05-20 11:29:20 -0500

    Update LoginController.php

--- codeingniter/app/Controllers/LoginController.php
+++ codeingniter/app/Controllers/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
         $rows = $this->login->countAllResults();
         $session = session();          
         if($rows==1){
-            return view('success');
             return view('user_id');
             return view('passowrd');
         }else{

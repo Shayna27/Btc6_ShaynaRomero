b957524867cb9c137edc02ddfdc35af19d6148a4
Shayna27 <61741756+Shayna27@users.noreply.github.com>
2021-05-20 10:45:45 -0500

    Update LoginController.php

--- codeingniter/app/Controllers/LoginController.php
+++ codeingniter/app/Controllers/LoginController.php
@@ -19,15 +19,18 @@ class LoginController extends Controller
     }      
 
     public function login(){
-          
+        
+        $data['user_name']="user_id"
+        $data['password']="password"   
         $data = array('user_name'=>$this->request->getVar('user_id'),'password'=>md5($this->request->getVar('password')));       
         $user =  $this->login->where($data); 
         $rows = $this->login->countAllResults();
         $session = session();          
         if($rows==1){
             return view('success');
-            $data['user_name']="user_id"
-            $data['password']="password"    
+            return view('"user_id"');
+            return view('password');
+               
         }else{
             $session->setFlashdata('msg', 'Invalid User');
             return view('login');
